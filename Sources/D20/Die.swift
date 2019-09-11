ffb17e07772b22eb2fea12735c53fafbc21600c7
Kilian Koeltzsch <me@kilian.io>
2019-09-11 11:03:50 +0200

    Use Regex-based replacement & NSExpression

--- Sources/D20/Die.swift
+++ Sources/D20/Die.swift
@@ -9,15 +9,29 @@ public struct D {
         Double(faces + 1) / 2
     }
 
-    public func roll() -> RollResult {
-        let roll = Int.random(in: 1...faces)
+    public func roll(mode: RollMode = .random) -> RollResult {
+        let roll = calc(mode: mode)
         return RollResult(description: "\(roll)", result: roll)
+    }
+
+    private func calc(mode: RollMode) -> Int {
+        switch mode {
+        case .random:
+            return Int.random(in: 1...faces)
+        case .max:
+            return faces
+        case .half:
+            return faces / 2
+        case .average:
+            // FIXME
+            return Int(Double(faces + 1) / 2)
+        }
     }
 }
 
 public extension Array where Element == D {
-    func roll() -> RollResult {
-        let rolls = self.map { $0.roll() }
+    func roll(mode: RollMode = .random) -> RollResult {
+        let rolls = self.map { $0.roll(mode: mode) }
 
         let description = rolls.map { $0.description }.joined(separator: " + ")
         let result = rolls.reduce(0) { $0 + $1.result }
--- Sources/D20/Roll.swift
+++ Sources/D20/Roll.swift
@@ -1,34 +1,66 @@
-import Parsel
+import class Foundation.NSExpression
+import Regex
 
 public struct Roll {
     public let formula: String
-//    private let term: Foo
 
     public init?(_ formula: String) {
         guard !formula.isEmpty else { return nil }
         self.formula = formula
-
-
     }
 
     public var max: Int {
-        return 1
+        roll(mode: .max).result
     }
 
     public var half: Int {
-        return 1
+        roll(mode: .half).result
     }
 
     public var average: Double {
         return 1.0
+//        return roll(mode: .average)
     }
 
-    public func roll() -> RollResult {
-        fatalError("not yet implemented")
+    public func roll(mode: RollMode = .random) -> RollResult {
+        let expression = rollDiceRolls(in: self.formula, mode: mode)
+        let mathExpression = NSExpression(format: expression)
+        guard let result = mathExpression.expressionValue(with: nil, context: nil) as? Int else {
+            fatalError("Failed to compute '\(expression)'")
+        }
+        return RollResult(description: expression, result: result)
+    }
+
+    private func rollDiceRolls(in formula: String, mode: RollMode) -> String {
+        let dieRegex = Regex(#"(\d+)?[dw](\d+)"#)
+        let matches = dieRegex.allMatches(in: formula)
+        let results = matches.map { match -> Int in
+            // FIXME: This unwrapping is ridiculous.
+            let diceCount = Int(match.captures.first! ?? "1")!
+            let value = Int(match.captures.last!!)!
+
+            let dice = Array<D>(repeating: D(value), count: diceCount)
+            return dice.roll(mode: mode).result
+        }
+        var copy = formula
+        for (idx, match) in matches.enumerated() {
+            copy = copy.replacingFirst(matching: dieRegex, with: String(results[idx]))
+        }
+
+        print(copy)
+
+        return copy
     }
 }
 
 public struct RollResult {
     public let description: String
     public let result: Int
+}
+
+public enum RollMode {
+    case random
+    case max
+    case half
+    case average
 }
